236261b9d6a6344cc6b7d2ff9eeb8722b6345413
fdesimini <fdesimini@users.noreply.github.com>
2015-05-13 12:50:21 -0400

    Initial Commit

---
+++ FindMyFriends/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindMyFriends
+//
+//  Created by Frank Desimini on 2015-05-13.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FindMyFriends/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FindMyFriends
+//
+//  Created by Frank Desimini on 2015-05-13.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
