d1f21ca5410ed49b0355b977d5274e9115787ffe
fdesimini <fdesimini@users.noreply.github.com>
2015-05-15 13:44:12 -0400

    withKwame

--- NewUserViewController.m
+++ NewUserViewController.m
@@ -49,68 +49,72 @@
     
     
     
-    
-    [self processFieldEntries];
+//    PFUser *createUser = [PFUser user];
+//    createUser.username = _createUsernameTextfield.text;
+//    createUser.password = _createPasswordTextfield.text;
+//    [createUser saveInBackground];
+//
+//    [self processFieldEntries];
     
 }
 
 #pragma mark Sign Up
 
-- (void)processFieldEntries {
-    // Check that we have a non-zero username and passwords.
-    // Compare password and passwordAgain for equality
-    // Throw up a dialog that tells them what they did wrong if they did it wrong.
-    
-    NSString *username = self.createUsernameTextfield.text;
-    NSString *password = self.createPasswordTextfield.text;
-    NSString *passwordAgain = self.confirmPasswordTextfield.text;
-    NSString *errorText = @"Please ";
-    NSString *usernameBlankText = @"enter a username";
-    NSString *passwordBlankText = @"enter a password";
-    NSString *joinText = @", and ";
-    NSString *passwordMismatchText = @"enter the same password twice";
-    
-    BOOL textError = NO;
-    
-    // Messaging nil will return 0, so these checks implicitly check for nil text.
-    if (username.length == 0 || password.length == 0 || passwordAgain.length == 0) {
-        textError = YES;
-        
-        // Set up the keyboard for the first field missing input:
-        if (passwordAgain.length == 0) {
-            [self.confirmPasswordTextfield becomeFirstResponder];
-        }
-        if (password.length == 0) {
-            [self.createPasswordTextfield becomeFirstResponder];
-        }
-        if (username.length == 0) {
-            [self.createPasswordTextfield becomeFirstResponder];
-        }
-        
-        if (username.length == 0) {
-            errorText = [errorText stringByAppendingString:usernameBlankText];
-        }
-        
-        if (password.length == 0 || passwordAgain.length == 0) {
-            if (username.length == 0) { // We need some joining text in the error:
-                errorText = [errorText stringByAppendingString:joinText];
-            }
-            errorText = [errorText stringByAppendingString:passwordBlankText];
-        }
-    } else if ([password compare:passwordAgain] != NSOrderedSame) {
-        // We have non-zero strings.
-        // Check for equal password strings.
-        textError = YES;
-        errorText = [errorText stringByAppendingString:passwordMismatchText];
-        [self.createPasswordTextfield becomeFirstResponder];
-    }
-    
-    if (textError) {
-        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:errorText message:nil delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-        [alertView show];
-        return;
-    }
-    
+//- (void)processFieldEntries {
+//    // Check that we have a non-zero username and passwords.
+//    // Compare password and passwordAgain for equality
+//    // Throw up a dialog that tells them what they did wrong if they did it wrong.
+//    
+//    NSString *username = self.createUsernameTextfield.text;
+//    NSString *password = self.createPasswordTextfield.text;
+//    NSString *passwordAgain = self.confirmPasswordTextfield.text;
+//    NSString *errorText = @"Please ";
+//    NSString *usernameBlankText = @"enter a username";
+//    NSString *passwordBlankText = @"enter a password";
+//    NSString *joinText = @", and ";
+//    NSString *passwordMismatchText = @"enter the same password twice";
+//    
+//    BOOL textError = NO;
+//    
+//    // Messaging nil will return 0, so these checks implicitly check for nil text.
+//    if (username.length == 0 || password.length == 0 || passwordAgain.length == 0) {
+//        textError = YES;
+//        
+//        // Set up the keyboard for the first field missing input:
+//        if (passwordAgain.length == 0) {
+//            [self.confirmPasswordTextfield becomeFirstResponder];
+//        }
+//        if (password.length == 0) {
+//            [self.createPasswordTextfield becomeFirstResponder];
+//        }
+//        if (username.length == 0) {
+//            [self.createPasswordTextfield becomeFirstResponder];
+//        }
+//        
+//        if (username.length == 0) {
+//            errorText = [errorText stringByAppendingString:usernameBlankText];
+//        }
+//        
+//        if (password.length == 0 || passwordAgain.length == 0) {
+//            if (username.length == 0) { // We need some joining text in the error:
+//                errorText = [errorText stringByAppendingString:joinText];
+//            }
+//            errorText = [errorText stringByAppendingString:passwordBlankText];
+//        }
+//    } else if ([password compare:passwordAgain] != NSOrderedSame) {
+//        // We have non-zero strings.
+//        // Check for equal password strings.
+//        textError = YES;
+//        errorText = [errorText stringByAppendingString:passwordMismatchText];
+//        [self.createPasswordTextfield becomeFirstResponder];
+//    }
+//    
+//    if (textError) {
+//        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:errorText message:nil delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+//        [alertView show];
+//        return;
+//    }
+
     // Everything looks good; try to log in.
 //    PAWActivityView *activityView = [[PAWActivityView alloc] initWithFrame:CGRectMake(0.f, 0.f, self.view.frame.size.width, self.view.frame.size.height)];
 //    UILabel *label = activityView.label;
@@ -151,7 +155,7 @@
 //        [self.delegate newUserViewControllerDidSignup:self];
 //    }];
     
-}
+//}
 
 - (IBAction)cancel:(id)sender {
     
